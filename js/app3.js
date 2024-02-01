9f71e051f74f43f0673d3213017ecf4ae48127d5
Mariia5pon <mariia5pon@gmail.com>
2024-02-01 15:27:42 +0100

    add residue of div to app3

--- js/app3.js
+++ js/app3.js
@@ -3,3 +3,19 @@
  */
 let a = parseFloat(prompt("Введіть число a:"));
 let b = parseFloat(prompt("Введіть число b:"));
+
+if (!isNaN(a) && !isNaN(b)) {
+    if (b % a === 0) {
+        document.write('a є дільником b<br>');
+    } else {
+        document.write('a не є дільником b<br>');
+    }
+
+    if (a % b === 0) {
+        document.write('b є дільником a<br>');
+    } else {
+        document.write('b не є дільником a<br>');
+    }
+} else {
+    document.write("Введено некоректні числа.");
+}
