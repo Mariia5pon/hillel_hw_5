1baf785f049a53dd401b7c1dd56badc99da2e48b
Mariia5pon <mariia5pon@gmail.com>
2024-02-06 14:55:12 +0100

    app8 ready

--- js/app8.js
+++ js/app8.js
@@ -1,3 +1,23 @@
 /*
 8. Определить, является ли заданное шестизначное число зеркальным? (123321, 147741)
  */
+let number = prompt("Введіть шестизначне число:");
+
+if (number.length !== 6 || isNaN(parseInt(number))) {
+    console.log("Будь ласка, введіть шестизначне число.");
+} else {
+    let digit1 = parseInt(number.charAt(0));
+    let digit2 = parseInt(number.charAt(1));
+    let digit3 = parseInt(number.charAt(2));
+    let digit4 = parseInt(number.charAt(3));
+    let digit5 = parseInt(number.charAt(4));
+    let digit6 = parseInt(number.charAt(5));
+
+    let isMirror = digit1===digit6 && digit2===digit5 && digit3===digit4;
+
+    if (isMirror) {
+        console.log("Число є зеркальним.");
+    } else {
+        console.log("Число не є зеркальним.");
+    }
+}
