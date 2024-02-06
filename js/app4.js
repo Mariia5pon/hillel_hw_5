584653dbdbb728098ac6e5f42b7f1336ca69e991
Mariia5pon <mariia5pon@gmail.com>
2024-02-06 13:48:03 +0100

    app4 ready

--- js/app4.js
+++ js/app4.js
@@ -1,3 +1,11 @@
 /*
 4. Дано число. Определить, заканчивается оно четной цифрой или нечетной? Вывести последнюю цифру.
  */
+let number = prompt("Введіть число:");
+
+let lastDigit = parseInt(number.charAt(number.length - 1));
+
+let message = (lastDigit % 2 === 0) ? "Остання цифра є парною." : "Остання цифра є непарною.";
+
+console.log("Остання цифра:", lastDigit);
+console.log(message);
