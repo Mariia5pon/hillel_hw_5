4fbd034c8395737eae01e55e15a594bd10601b41
Mariia5pon <mariia5pon@gmail.com>
2024-02-06 14:08:37 +0100

    app6.a ready

--- js/app6.js
+++ js/app6.js
@@ -4,3 +4,16 @@
     b. Определить, кратна ли сумма его цифр пяти.
     c. Определить является ли произведение его цифр больше 100.
  */
+let number = prompt("Введіть трьохзначне число:");
+
+if (number.length !== 3 || isNaN(parseInt(number))) {
+    console.log("Будь ласка, введіть трьохзначне число.");
+} else {
+    let digit1 = parseInt(number.charAt(0));
+    let digit2 = parseInt(number.charAt(1));
+    let digit3 = parseInt(number.charAt(2));
+
+    let sum = digit1 + digit2 + digit3;
+
+    document.write(sum % 2 === 0 ? "Сума цифр є парною." : "Сума цифр є непарною.")
+}
