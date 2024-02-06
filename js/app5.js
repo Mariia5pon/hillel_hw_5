edfd7839e41b382377cf0612a71f0e56ccd42633
Mariia5pon <mariia5pon@gmail.com>
2024-02-06 14:00:50 +0100

    app5 ready

--- js/app5.js
+++ js/app5.js
@@ -1,3 +1,22 @@
 /*
 5. Дано двузначное число. Определить, какая из его цифр больше: первая или вторая?
  */
+let number = prompt("Введіть двозначне число:");
+
+if (number.length !== 2 || isNaN(parseInt(number))) {
+    console.log("Будь ласка, введіть двозначне число.");
+} else {
+    let firstDigit = parseInt(number.charAt(0));
+    let secondDigit = parseInt(number.charAt(1));
+
+    switch (true) {
+        case firstDigit > secondDigit:
+            console.log("Перша цифра більша за другу.");
+            break;
+        case firstDigit < secondDigit:
+            console.log("Друга цифра більша за першу.");
+            break;
+        default:
+            console.log("Цифри рівні.");
+    }
+}
